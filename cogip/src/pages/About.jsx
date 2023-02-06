6e65e6fddb05afcf8a776d4e6da35782d86e28db
Benjamin Porsont <porsont.benjamin@gmail.com>
2023-02-06 16:59:58 +0100

    modif

---
+++ cogip/src/pages/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error excepturi
+        quidem minima iure, illo corrupti quos distinctio asperiores quia
+        molestiae itaque vero natus similique labore repudiandae aut
+        necessitatibus quam. Beatae.
+      </h1>
+    </div>
+  );
+};
+
+export default About;
